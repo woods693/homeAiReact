5d8c9b0acd1574f64a39516dbdc088982b4ff23a
woods693 <chang06151995@gmail.com>
2018-10-17 14:35:41 -0700

    devices and map working

--- src/components/devices/devices.js
+++ src/components/devices/devices.js
@@ -9,26 +9,43 @@ export default class Devices extends Component {
 
     }
     this.logger = this.logger.bind(this);
+    this.displayDevices = this.displayDevices.bind(this);
   };
 
   logger(){
     console.log(this.props.devices)
+    console.log(this.props.floor)
   };
+  //
 
   displayDevices(){
+    //dont have Motion Detector, Combination Switch, Combination Outlet, Combination Speaker
     let allDevices = []
+    //should set a variable instead
     this.props.devices.forEach(function(each){
-      allDevices.push(
-        <div key={each.ID} className="deviceList">
-          {each.Name}
-        </div>
-      );
-    })
+      var left = ('' + each.X) + 'px';
+      var top = ('' + each.Y) + 'px';
+      //console.log(this.props.floor)
+      if(each.Floor_ID == this.props.floor){
+        //console.log(each.Type_Name)
+        var status = each.Status_Name.toLowerCase();
+        //console.log(status);
+        allDevices.push(
+          <div style={{left: left,
+            top: top ,
+            position: 'absolute',
+            margin: '0 auto'}}
+            key={each.ID} className="deviceList">
+            <img src={`../../public/images/mapIcons/${each.Type_Name}_${status}40.png`}/>
+          </div>
+        );
+      }
+    }.bind(this));
     return allDevices;
   };
   render() {
     return (
-      <div>
+      <div className='devices' style={{position:'relative'}}>
         {this.displayDevices()}
       </div>
     )
--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -7,19 +7,26 @@ import Devices from '../../components/devices/devices.js';
 
 class MainPage extends Component{
   constructor(props){
-  super(props);
-  this.state={
-    category_devices: this.props.devices,
-    selected_floor: "../../public/images/floors/floor1_big.png",
-  };
+    super(props);
+    this.state={
+      category_devices: this.props.devices,
+      selected_floor: "../../public/images/floors/floor1_big.png",
+      selected_floor_number: 1
+    };
+    //this.selectType = this.selectType.bind(this);
 };
 
-  selectType = (event) => {
+  selectType(target){
       //console.log('Before', this.state.category_devices);
-      const target = event.target.getAttribute('data-key');
+      //const target = event.target.getAttribute('data-key');
+      //console.log(this.state.selected_floor_number)
       const all = this.props.devices;
-
-      const result = all.filter(each => each.parent_id == target);
+      let result = []
+      if (target == 0){
+        result = all;
+      }else{
+        result = all.filter(each => each.parent_id == target);
+      }
       this.setState({
         category_devices: result
       });
@@ -27,8 +34,10 @@ class MainPage extends Component{
 
   switchFloors(floor){
     const selected = `../../public/images/floors/floor${floor}_big.png`;
+    //console.log(floor);
     this.setState({
       selected_floor: selected,
+      selected_floor_number: floor
     })
   };
 
@@ -52,7 +61,8 @@ class MainPage extends Component{
 
   createTypes = () => {
     let all = [];
-
+    //console.log(this.props.devices)
+    //should change this but 6 device types so it works
     for (let i = 1; i < 6; i++){
       all.push(
         <button
@@ -61,11 +71,10 @@ class MainPage extends Component{
           aria-pressed="true"
           key={`type${i}`}
           data-key={`${i}`}
-          onClick={this.selectType.bind(this)}>
+          onClick={() => this.selectType(i)}>
             <img className={`icon${i} iconTypes`}
             src={`../../public/images/type${i}.png`}/>
         </button>
-
       )
     }
     return all
@@ -73,6 +82,14 @@ class MainPage extends Component{
 
   render() {
     const imageAddr = '../../public/images/type5.png';
+    var floorStyle = {
+      background: `url(${this.state.selected_floor})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height: '850px',
+      width: '1500px',
+      margin: '0 auto'
+    }
     return(
       <div className='container-table mainPage'>
         <div className='iconBar row main-align'>
@@ -83,10 +100,11 @@ class MainPage extends Component{
                 {this.createFloors()}
               </ul>
           </div>
-          <div className='deviceBar'>
-            <img src={this.state.selected_floor}/>
-            <Devices devices={this.state.category_devices}/>
-          </div>
+            <div className='text-center' style={floorStyle}>
+              <Devices
+                devices={this.state.category_devices}
+                floor={this.state.selected_floor_number}/>
+            </div>
         </div>
       </div>
     );
